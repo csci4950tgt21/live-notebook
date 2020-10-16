94a871f2a05651fde122be8a7eccd8aa667522a1
Evan Gunn <egunn93@gmail.com>
2020-10-16 17:27:45 -0500

    RegexConcat(regex_manager.ts,extension.ts) - Renamed regexManager.ts to regex_manager.ts - Added in a function for regex concatentation to concatenate all regexes from the configuration - matchToken function now only takes a token parameter and tests on all known types of regexes, and returns the string name of the type of token.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { RegexManager } from "./regex_manager";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,6 +20,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from live-notebook!');
 	});
+
+	var myManager: RegexManager = new RegexManager();
+	let myNewRegex = myManager.concatRegex();
+
+	console.log(myNewRegex);
 
 	context.subscriptions.push(disposable);
 }
---
+++ src/regex_manager.ts
@@ -0,0 +1,78 @@
+import * as vscode from 'vscode';
+
+/**
+ * The NamedRegex type holds both a regular expression, and the name
+ * of the regular expression's type, such as "URL" or "EMAIL".
+ */
+type NamedRegex = {
+    name: string;
+    regex: RegExp;
+};
+
+/**
+ * The regex manager is meant to manage all regular expressions
+ * used to identify types of strings, such as URL or email. It
+ * loads regular expressions and their types "name" in pairs from
+ * the JSON settings file. (in package.json under "configuration")
+ */
+export class RegexManager {
+    // The array of regex and type name pairs.
+    private regexes: Array<NamedRegex> = [];
+
+    // The constructor initalizes the regexes array by loading data
+    // from user settings.
+    constructor() {
+        this.loadConfig();
+    }
+
+    /**
+     * Load names and regexes from user settings
+     */
+    public loadConfig() {
+        // TODO PLACEHOLDER CODE, waiting on final schema of user settings to be decided!
+        // DO NOT COPY THESE REGEXES INTO CONFIG FILE, WE SHOULD FIND STANDARDIZED VERSIONS ONLINE, AND CITE SOURCES!
+        this.regexes = [ { name: "EMAIL", regex: /(?<=\s|^)\w+@\w+\.\w+(?=\s|$)/ },
+                         { name: "URL", regex: /(?<=\s|^)https?:\/\/[\S]+|(?<=\s|^)\w+\.[a-zA-Z]+[\S]*(?=\s|$)/},
+                         { name: "IP", regex: /(?<=\s|^)(\d{1,3}\.){3}\d{1,3}(?=\s|$)|(?<=\s|^)(\w{1,4}\:){7}\w{1,4}(?=\s|$)|(?<=\s|^)\w{1,4}\:(\w{0,4}\:){5}\w{1,4}(?=\s|$)/}
+                       ];
+    }
+
+    /**
+     * Check if a token matches with a named regular expression pattern,
+     * such as email, URL, IP, etc. Meant to be used when checking if a token
+     * can be called by an API.
+     * @param token The text token to be matched.
+     * @returns True if passed token is matched, false otherwise.
+     */
+    public matchToken(token: string) {
+        this.regexes.forEach(element => {
+            if (token.match(element.regex) != null) {
+                return element.name;
+            }
+        });
+        return false;
+    }
+
+    /**
+     * The concatenated regular expression consisting of all
+     * regular expressions in the configuration file.
+     */
+    public concatRegex() {
+        var newRegex: string = "/";
+        var i = 0;
+        var end = this.regexes.length;
+        this.regexes.forEach(element => {
+            // Add in the | character between each regex
+            if (i !== 0 && i < end) {
+                newRegex+="|";
+            }
+            i++;
+            //regex.source will remove the surrounding // from the regex
+            newRegex += element.regex.source;
+        });
+        // When finished, add the last / to the new regex,
+        // if no regexes exist this will return //
+        newRegex += "/";
+        return newRegex;
+    }
+}
