ff60aad1b5145c077d3db9102dc8520b5093f2d4
Evan Gunn <egunn93@gmail.com>
2020-10-28 23:14:35 -0500

    ConfigLoading(config_manager.ts,config_types.ts,regex_manager.ts,package.json) - Modified schema in package.json to allow for an array of regexes that   have a type field and a regex field. Removed regular expressions from   package.json for now, as they do not work. - Added config_manager.ts class, this class manages all loading of   configuration data from package.json, and currently has a method to   load in regexes. - Modified regex_manager.ts to load in regexes using config_manager.ts. - Moved TypedRegex to config_types, so that it can be accessed from both   config_manager.ts and regex_manager.ts.

---
+++ src/config_manager.ts
@@ -0,0 +1,40 @@
+import * as vscode from 'vscode';
+
+/**
+ * The config manager class manages
+ * all access to and from the settings/configuration.
+ * The configuration can be found in package.json.
+ */
+export class ConfigManager {
+    // Singleton instance of the config manager
+    private static myConfigManager: ConfigManager;
+
+    /**
+     * @returns The singleton instance of ConfigManager
+     */
+    public static getConfigManager () {
+        if (!ConfigManager.myConfigManager) ConfigManager.myConfigManager = new ConfigManager();
+        return ConfigManager.myConfigManager;
+    }
+
+    /**
+     * Access the config file in package.json
+     * and load in the regular expressions.
+     * @returns An array of TypedRegex
+     */
+    public getTypedRegexes() {
+        // Access the configuration
+        let regexArray : any = vscode.workspace.getConfiguration('live-notebook').get('regexes');
+
+        // Create a new array of typed regular expressions,
+        // and assign them to the configuration values
+        let regexes = new Array<TypedRegex>(regexArray.length);
+        for (let i = 0; i < regexArray.length; i++) {
+            regexes[i] = {type: regexArray[i].type, regex: regexArray[i].regex};
+        }
+        return regexArray;
+    }
+
+    // TODO API config accessing code
+}
+export default ConfigManager;
--- src/regex_manager.ts
+++ src/regex_manager.ts
@@ -1,23 +1,15 @@
 import * as vscode from 'vscode';
-
-/**
- * The NamedRegex type holds both a regular expression, and the name
- * of the regular expression's type, such as "URL" or "EMAIL".
- */
-type NamedRegex = {
-    name: string;
-    regex: RegExp;
-};
+import ConfigManager from './config_manager';
 
 /**
  * The regex manager is meant to manage all regular expressions
  * used to identify types of strings, such as URL or email. It
- * loads regular expressions and their types "name" in pairs from
+ * loads regular expressions and their type in pairs from
  * the JSON settings file. (in package.json under "configuration")
  */
 export class RegexManager {
-    // The array of regex and type name pairs.
-    private regexes: Array<NamedRegex> = [];
+    // The array of regex and type pairs.
+    private regexes: Array<TypedRegex> = [];
 
     // The constructor initalizes the regexes array by loading data
     // from user settings.
@@ -26,19 +18,15 @@ export class RegexManager {
     }
 
     /**
-     * Load names and regexes from user settings
+     * Load regexes and their types from user settings
      */
     public loadConfig() {
-        // TODO PLACEHOLDER CODE, waiting on final schema of user settings to be decided!
-        // DO NOT COPY THESE REGEXES INTO CONFIG FILE, WE SHOULD FIND STANDARDIZED VERSIONS ONLINE, AND CITE SOURCES!
-        this.regexes = [ { name: "EMAIL", regex: /(?<=\s|^)\w+@\w+\.\w+(?=\s|$)/ },
-                         { name: "URL", regex: /(?<=\s|^)https?:\/\/[\S]+|(?<=\s|^)\w+\.[a-zA-Z]+[\S]*(?=\s|$)/},
-                         { name: "IP", regex: /(?<=\s|^)(\d{1,3}\.){3}\d{1,3}(?=\s|$)|(?<=\s|^)(\w{1,4}\:){7}\w{1,4}(?=\s|$)|(?<=\s|^)\w{1,4}\:(\w{0,4}\:){5}\w{1,4}(?=\s|$)/}
-                       ];
+        // Load the regexes in the ConfigManager class
+        this.regexes = ConfigManager.getConfigManager().getTypedRegexes();
     }
 
     /**
-     * Check if a token matches with a named regular expression pattern,
+     * Check if a token matches with a regular expression pattern,
      * such as email, URL, IP, etc. Meant to be used when checking if a token
      * can be called by an API.
      * @param token The text token to be matched.
@@ -47,7 +35,7 @@ export class RegexManager {
     public matchToken(token: string) : string | undefined {
         for (let i = 0; i < this.regexes.length; i++) {
             if (token.match(this.regexes[i].regex)) {
-                return this.regexes[i].name;
+                return this.regexes[i].type;
             }
         }
         return undefined;
