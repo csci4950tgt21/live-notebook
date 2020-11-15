ad26311215ff22fceff54938bfb0a6aa7a6320c0
Evan Gunn <egunn93@gmail.com>
2020-11-14 18:46:29 -0600

    Added Strategy class for APIs

---
+++ src/api_strategy.ts
@@ -0,0 +1,30 @@
+/**
+ * An API strategy 
+ */
+export abstract class APIStrategy {
+    // The JSON for the API
+    private apiJSON : JSON;
+
+    constructor(apiJSON : JSON) {
+        this.apiJSON = apiJSON;
+    }
+
+    /**
+     * Get the common data model response
+     * @param token The parsed text token, for example a URL.
+     */
+    public getCDMresponse(token : string) {
+        return APIStrategy.mapResponseToCDM(this.getReponse(token));
+    }
+
+    // TODO
+    private getReponse(token : string) : JSON | undefined {
+        // TODO
+        return undefined;
+    }
+
+    // TODO
+    private static mapResponseToCDM(reponse : JSON | undefined) {
+        // TODO
+    }
+}
