a0c7f44c59e13c0cfc50aec8dddcee157e3a4995
Evan Gunn <egunn93@gmail.com>
2020-12-02 16:06:36 -0600

    Removed leftover line from testing

--- src/virus_total_strategy.ts
+++ src/virus_total_strategy.ts
@@ -44,8 +44,6 @@ export default class VirusTotalStrategy extends APIStrategy {
             }
         }
 
-        Promise.resolve(resolve);
-
         return axios
             .post(api_url, formData, virus_total_header).then(resolve, (err: any) => {
             console.error(err);
