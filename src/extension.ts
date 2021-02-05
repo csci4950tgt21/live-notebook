abc41a8457de9cde116a12379f4addb19e122fd0
wu000168 <wu000168@umn.edu>
2021-02-04 19:10:55 -0600

    String Underlining - No Differenciation

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,20 @@ import * as vscode from 'vscode';
 import { TokenMatcher } from "./token_matcher"
 import NotebookHoverProvider from "./hover_provider";
 import APICalls from './api_calls';
-import {ExtendedResultsProvider} from './extended_results_provider'
+import { ExtendedResultsProvider } from './extended_results_provider'
 import { SidePanels } from "./side_panels";
+import DiagnositcsProvider from './diagnostics_provider';
+
+let diagnosticCollection: vscode.DiagnosticCollection;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let globalMatcher: TokenMatcher = new TokenMatcher();
+	let apiCalls: APICalls = new APICalls();
+
 	context.subscriptions.push(
-		vscode.languages.registerHoverProvider("plaintext", new NotebookHoverProvider(new TokenMatcher(), new APICalls()))
+		vscode.languages.registerHoverProvider("plaintext", new NotebookHoverProvider(globalMatcher, apiCalls))
 	);
 	context.subscriptions.push(
 		vscode.languages.registerCodeActionsProvider('plaintext', new ExtendedResultsProvider())
@@ -24,7 +30,10 @@ export function activate(context: vscode.ExtensionContext) {
 	};
 
 	context.subscriptions.push(vscode.commands.registerCommand(command, commandHandle));
-	
+
+	// context.subscriptions.push(vscode.getDisposable());
+	let highlights = vscode.languages.createDiagnosticCollection('highlights');
+	new DiagnositcsProvider(globalMatcher).subscribeToDocumentChanges(context, highlights);
 }
 
 // this method is called when your extension is deactivated
