c345511d7e9aa595a41c244addcac7f3f70adac8
Z <wu000168@umn.edu>
2021-03-08 16:46:13 -0600

    Diagnostics Tests (#19)
    * Diagnostics Tests
    
    * Diagnostics Test Suite Name Fix

--- src/diagnostics_provider.ts
+++ src/diagnostics_provider.ts
@@ -1,38 +1,41 @@
-import { Diagnostic } from "vscode";
-
 import * as vscode from 'vscode';
 import TokenMatcher from "./token_matcher";
 
 export default class DiagnositcsProvider {
+    private context: vscode.ExtensionContext;
     private matcher: TokenMatcher;
+    private highlights: vscode.DiagnosticCollection;
 
-    constructor(matcher: TokenMatcher) {
+    constructor(context: vscode.ExtensionContext, matcher: TokenMatcher) {
+        this.context = context;
         this.matcher = matcher;
+        this.highlights = vscode.languages.createDiagnosticCollection('highlights');
+        this.subscribeToDocumentChanges();
     }
 
-    subscribeToDocumentChanges(context: vscode.ExtensionContext, highlights: vscode.DiagnosticCollection): void {
+    subscribeToDocumentChanges(): void {
         if (vscode.window.activeTextEditor) {
-            this.refreshDiagnostics(vscode.window.activeTextEditor.document, highlights);
+            this.refreshDiagnostics(vscode.window.activeTextEditor.document);
         }
-        context.subscriptions.push(
+        this.context.subscriptions.push(
             vscode.window.onDidChangeActiveTextEditor(editor => {
                 if (editor) {
-                    this.refreshDiagnostics(editor.document, highlights);
+                    this.refreshDiagnostics(editor.document);
                 }
             })
         );
 
-        context.subscriptions.push(
-            vscode.workspace.onDidChangeTextDocument(editor => this.refreshDiagnostics(editor.document, highlights))
+        this.context.subscriptions.push(
+            vscode.workspace.onDidChangeTextDocument(editor => this.refreshDiagnostics(editor.document))
         );
 
-        context.subscriptions.push(
-            vscode.workspace.onDidCloseTextDocument(doc => highlights.delete(doc.uri))
+        this.context.subscriptions.push(
+            vscode.workspace.onDidCloseTextDocument(doc => this.highlights.delete(doc.uri))
         );
 
     }
 
-    refreshDiagnostics(document: vscode.TextDocument, highlights: vscode.DiagnosticCollection): void {
+    refreshDiagnostics(document: vscode.TextDocument): void {
         const diagnostics: vscode.Diagnostic[] = [];
         for (var i = 0; i < document.lineCount; i++) {
             var line: string = document.lineAt(i).text;
@@ -40,14 +43,14 @@ export default class DiagnositcsProvider {
                 s => {
                     var li = line.indexOf(s);
                     while (li != -1) {
-                        const diagnostic: Diagnostic = new Diagnostic(new vscode.Range(i, li, i, li + s.length),
-                                                                     (this.matcher.matchToken(s) ?? "undefined type") + " - " + s, vscode.DiagnosticSeverity.Information);
+                        const diagnostic: vscode.Diagnostic = new vscode.Diagnostic(new vscode.Range(i, li, i, li + s.length),
+                            (this.matcher.matchToken(s) ?? "undefined type") + " - " + s, vscode.DiagnosticSeverity.Information);
                         diagnostics.push(diagnostic);
                         li = line.indexOf(s, li + 1);
                     }
                 }
             );
         }
-        highlights.set(document.uri, diagnostics);
+        this.highlights.set(document.uri, diagnostics);
     }
 }
